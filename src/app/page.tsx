f054e538a42a00879294a66c1483c81d1a91e2f3
shubhadamohite <shubhada.rocks11@gmail.com>
2025-06-12 16:17:24 -0700

    Initial setup

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+
+export default function Home() {
+  return (
+    <div className="flex p-4 gap-4">
+      <div className="flex-1 w-1/2 border p-4">
+        <h2 className="text-2xl font-bold mb-2">To-do List</h2>
+      </div>
+      <div className="flex-1 w-1/2 border p-4">
+        <h2 className="text-2xl font-bold mb-2">Pomodoro Timer</h2>
+      </div>
+    </div>
+  );
+}
